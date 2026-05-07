91946f08322f2f5a4630790520a35336d23efc51
Guruprakash-Mohan <gukash007@gmail.com>
2026-05-07 02:15:19 +0200

    Add booking client with create booking

---
+++ src/test/java/com/api/tests/clients/BookingClient.java
@@ -0,0 +1,23 @@
+package com.api.tests.clients;
+
+import com.api.framework.constants.ApiConstants;
+import com.api.framework.specifications.RequestSpecifications;
+import com.api.tests.models.request.BookingRequest;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Client for booking API operations.
+ */
+public class BookingClient {
+
+    private static final Logger log = LoggerFactory.getLogger(BookingClient.class);
+
+    public Response createBooking(BookingRequest request) {
+        log.info("POST {} body={}", ApiConstants.BOOKING_ENDPOINT, request);
+        return RequestSpecifications.baseSpec()
+                .body(request)
+                .post(ApiConstants.BOOKING_ENDPOINT);
+    }
+}
